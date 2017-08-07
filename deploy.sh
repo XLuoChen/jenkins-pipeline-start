998ded57a95d207954ff1ecc6756b34602d56091
XluoChen <cxluo@thoughtworks.com>
2017-08-07 14:31:49 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo -s
-cd /var && mkdir www && cd www && mkdir html
+#cd /var && mkdir www && cd www && mkdir html
 sudo cp -r /var/lib/jenkins/workspace/mypipeline /var/www/html
-service nginx restart
