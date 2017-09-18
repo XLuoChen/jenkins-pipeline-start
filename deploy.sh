93d2459c8cd976d4adc36cd42ffe4e948c508647
Chengxiu Luo <cxluo@thoughtworks.com>
2017-09-18 23:44:51 +0800

    copy directory

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 if [ ! -d /var/www/html ]; then
     sudo mkdir -p /var/www/html
 fi
-sudo cp -r /var/lib/jenkins/workspace/mypipeline/* /var/www/html
+sudo cp -r /var/lib/jenkins/workspace/mypipeline /var/www/html
